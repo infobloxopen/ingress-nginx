b592e90f389a38043e7a398f2aceb6615f974a09
Andrei Lavrov <alavrov@infoblox.com>
2020-10-15 12:31:08 +0300

    add  push nginx image

--- images/nginx/fips-test.sh
+++ images/nginx/fips-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export BASEIMAGE=${REGISTRY}/nginx-fips:${TAG}
+echo "Openssl fips test"
 docker run -it -e OPENSSL_FIPS=1 ${BASEIMAGE} openssl md5 /dev/null
 status=$?
-echo "Openssl fips test"
+
 if test $status -eq 0
 then
     echo "fips test failed"
